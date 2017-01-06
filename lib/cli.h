53acde711f9e6fbceb5f99de93744e61772ca1fd
Noémi Ványi <sitbackandwait@gmail.com>
2017-01-06 19:57:18 +0100

    cli-config: fix copyright
    Signed-off-by: Noémi Ványi <sitbackandwait@gmail.com>

--- lib/cli.h
+++ lib/cli.h
@@ -1,5 +1,5 @@
- /*
- * Copyright (c) 2016 Noemi Vanyi
+/*
+ * Copyright (c) 2016-2017 Noémi Ványi
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
